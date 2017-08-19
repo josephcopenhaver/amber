cbb440f9ddc9924588c2b9b0dd7115ce5794a7ef
Joe Copenhaver <joseph.copenhaver@gmail.com>
2017-08-19 00:18:22 -0500

    WIP2

--- src/amber/router/session/memcached_store.cr
+++ src/amber/router/session/memcached_store.cr
@@ -3,19 +3,20 @@ require "memcached"
 module Amber::Router::Session
   class MemcachedStore < AbstractStore
     getter store : Memcached::Client
-    property expire_seconds : Int32
+    property expires : Int32
     property key : String
-    property session_id : String
+    property session_id : String | Nil
     property session : Amber::Router::Session::SessionHash
     property cookies : Amber::Router::Cookies::Store
 
     forward_missing_to session
 
-    def initialize(@store, @cookies, @key, @expire_seconds = 120)
+    def initialize(@store, @cookies, @key, @expires = 120)
       @session_id = cookies.encrypted[key]
+      @session = init_session_from_store
     end
 
-    def session_from_store()
+    def init_session_from_store()
       if @session_id
         SessionHash.from_json(store.get("#{key}:#{session_id}") || "{}")
       else
@@ -25,19 +26,10 @@ module Amber::Router::Session
       SessionHash.new
     end
 
-    def session_to_store
-      session[:id] = id
-      cookies.encrypted.set(key, session_id, expires: expires_at, http_only: true)
-      store.set("#{key}:#{session_id}", session.to_json, expire: expire_seconds)
-      puts "new session saved"
-    end
-
     def id
       if @session_id
-        puts "existing id"
         session_id
       else
-        puts "New session id created"
         @session_id = SecureRandom.uuid
       end
     end
@@ -51,21 +43,19 @@ module Amber::Router::Session
         store.delete("#{key}:#{session_id}")
         @session_id = nil
       end
-      if @session
-        session.clear
-      end
+      session.clear
     end
 
     def update(hash : Hash(String, String))
-      current_session
       hash.each { |ikey, ivalue| session[ikey.to_s] = ivalue }
-      session_to_store
+      set_session
       session
     end
 
     def set_session
-      current_session
-      session_to_store
+      session[:id] = id
+      cookies.encrypted.set(key, session_id.to_s, expires: expires_at, http_only: true)
+      store.set("#{key}:#{session_id}", session.to_json, expire: expires)
     end
 
     def expires_at
@@ -73,18 +63,6 @@ module Amber::Router::Session
     end
 
     def current_session()
-      if ! @session
-        if @session_id
-          @session = session_from_store
-          if (! session.key?("id")) || (session[:id] != session_id)
-            # this should never happen, but I am a defensive programmer
-            session.clear
-            puts "Memcached fetch returned incorrect records"
-          end
-        else
-          @session = SessionHash.new
-        end
-      end
       session
     end
   end
